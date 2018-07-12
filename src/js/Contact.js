f2c21a33a0ed944bca2321e812f6cd3a134f02c4
Tomasz Czechlewski <tomasz.czechlewski@gmail.com>
2018-07-12 23:19:36 +0200

    few change in css style

--- src/js/Contact.js
+++ src/js/Contact.js
@@ -6,13 +6,20 @@ class Contact extends Component{
         return (
             <section className='contact container clearfix'>
                 <div className="sectionHeader">Kontakt</div>
-                <div>
-                    <form>
-                        <input type='email' placeholder="wpisz swój adres email"/>
-                        <input type='number' placeholder='wpisz swój numer telefonu'/>
-                        <textarea placeholder='wpisz wiadomość'/>
-                        <button></button>
-                    </form>
+                <div id='contactForm'>
+                    <div >
+                        <form>
+                            <input type='email' placeholder="wpisz swój adres email"/>
+                            <input type='number' placeholder='wpisz swój numer telefonu'/>
+                            <textarea placeholder='wpisz wiadomość'/>
+                            <button></button>
+                        </form>
+                    </div>
+                    <div id="contactDescription">
+                        Piotr <br/>
+                        tel: 6000 6000 6000<br/>
+                        email: jajaja@ja.pl
+                    </div>
                 </div>
             </section>
         )
